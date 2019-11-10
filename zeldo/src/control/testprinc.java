b6f957a91502f2dbf6cf2b749e68313c090de528
Delrieux <ro.delrieux@laposte.net>
2019-11-10 22:41:12 +0300

    a

---
+++ zeldo/src/control/testprinc.java
@@ -0,0 +1,12 @@
+package control;
+
+public class testprinc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(3);
+		System.out.print(3);
+
+	}
+
+}
