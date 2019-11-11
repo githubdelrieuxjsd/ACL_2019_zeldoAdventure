1562fea5617ea89694016a4a5edfccf94c03f34f
Linlupok <Linlupok@users.noreply.github.com>
2019-11-11 16:15:30 +0100

    Hero modifications Coordonees modifications Move interface created

--- zeldo/src/model/Coordonnees.java
+++ zeldo/src/model/Coordonnees.java
@@ -23,5 +23,5 @@ public class Coordonnees {
 	public int getY() {
 		return y;
 	}
-
+	
 }
--- zeldo/src/model/Hero.java
+++ zeldo/src/model/Hero.java
@@ -1,11 +1,38 @@
 package model;
 
 
-public class Hero extends Unit {
+public class Hero extends Unit implements Move{
 
-	public Hero() {
+	Coordonnees coord;
+
+
+	public Hero(Coordonnees c) {
 		this.setNom("Hero");
 		this.setImageURL("Image Zeldo");
+		this.coord = c;
 	}
+
+
+
+	public Coordonnees getCoord() {
+		return coord;
+	}
+
+
+
+	public void setCoord(Coordonnees coord) {
+		this.coord = coord;
+	}
+
+
+
+	public void move(Board board, Case nextCase) {
+		board.getBoard()[this.getCoord().getX()][this.getCoord().getY()].setUnit(new UnitVoid());
+		this.setCoord(nextCase.getCoordonnee());
+		board.getBoard()[nextCase.getCoordonnee().getX()][nextCase.getCoordonnee().getY()].setUnit(new Hero(nextCase.getCoordonnee()));
+
+	}
+
+
 
 }
