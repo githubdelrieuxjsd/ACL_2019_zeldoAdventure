6fc9907b5a59760f67179b418be39f04bcddee82
mAlrd <allard.matheo1@gmail.com>
2019-11-11 14:19:32 +0100

    Merge remote-tracking branch 'origin/matheo'
    Conflicts:
    	zeldo/src/control/Control.java
    	zeldo/src/model/Board.java
    	zeldo/src/model/Case.java
    	zeldo/src/model/Coordonnee.java
    	zeldo/src/model/PrincipaleTestModel.java
    	zeldo/src/vue/Principale.java
    
    Conflicts done.

---
+++ zeldo/src/model/PrincipaleTestModel.java
@@ -0,0 +1,25 @@
+package model;
+
+public class PrincipaleTestModel {
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> refs/remotes/origin/matheo
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Grass grass = new Grass();
+		System.out.println(grass.getNom());
+<<<<<<< HEAD
+
+		Board board = new Board(3,6);
+
+		board.toString();
+=======
+>>>>>>> refs/remotes/origin/matheo
+
+	}
+
+}
+
+
