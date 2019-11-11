f29b7b58161345bb961f7fb2f01ddd18b4f10d32
Delrieux <ro.delrieux@laposte.net>
2019-11-11 13:16:14 +0300

    David : ajout de la vue

---
+++ zeldo/src/vue/VueMenuStart.java
@@ -0,0 +1,74 @@
+package vue;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class VueMenuStart extends JPanel {
+
+	private JLabel titreJeuLbl;
+	private JButton playBtn;
+	private JButton exitBtn;
+	private VueJeu jeuPane;
+
+	public VueMenuStart() {
+		this.setLayout(null); // poser element n'importe ou
+		this.setBackground(Color.GREEN);
+		
+		ajout();
+	}
+
+	private void ajout() {
+		jeuPane= new VueJeu();
+		titreJeuLbl = new JLabel("Zeldo Adventure");
+		playBtn = new JButton("Start");
+		exitBtn = new JButton("Exit");
+		
+		titreJeuLbl.setBounds(250,250,300,70); //x,y,largeur,hauteur
+		titreJeuLbl.setFont(new Font("Segoe Script",Font.BOLD, 30)); //taille
+		this.add(titreJeuLbl);
+		
+		playBtn.setBounds(250,350,300,70);
+		playBtn.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				startJeu();
+			}
+		}); 
+		this.add(playBtn);		
+		
+		exitBtn.setBounds(700,10,90,50);
+		exitBtn.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//setBackground(Color.RED);
+				exitJeu();
+			}
+		}); 
+		this.add(exitBtn);	
+	}
+	
+	private void startJeu() {
+		remove();
+		Principale.getFrame().setContentPane(jeuPane); //donner panneau jeu dans principale frame
+		Principale.getFrame().pack();
+		this.updateUI(); //update principale frame
+		
+	}
+	
+	private void exitJeu() {
+		remove();
+		System.exit(0);
+	}
+	
+	private void remove() {
+		remove(titreJeuLbl);
+		remove(playBtn);
+		remove(exitBtn);
+	}
+	
+	
+}
